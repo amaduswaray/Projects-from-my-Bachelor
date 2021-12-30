75e70a9b52187118c65d3ced1cf8282b9078d4b6
Amadu Swaray <86655546+amaduswaray@users.noreply.github.com>
2021-12-30 15:06:38 +0100

    Add files via upload

---
+++ Object Oriented Programming/Labyrinth/Threads/SortRute.java
@@ -0,0 +1,33 @@
+//lager sort rute
+class SortRute extends Rute{
+
+//gjor alle svarte ruter sin rort til 1, slik at de returnerer naar de kaller gaa
+  public SortRute(int r, int k){
+    super(r,k);
+    rort = 1;
+  }
+
+  @Override
+  public char tilTegn(){
+    return '#';
+  }
+
+  public String toString(){
+    return "#";
+  }
+
+
+
+  public void gaa(int rad, int kol, String vei){
+    return;
+  }
+
+  /*public void run(){
+    return;
+  }*/
+
+
+
+
+
+}
